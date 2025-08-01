f7aaab19b6864561993feb476fe6ac6d389fc0d0
Parham Majd <parham@parhammajd.co.uk>
2025-08-01 05:06:19 +0100

    Initial Commit

---
+++ SupabaseSwift/SupabaseSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseSwiftApp.swift
+//  SupabaseSwift
+//
+//  Created by Parham on 01/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SupabaseSwiftTests/SupabaseSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseSwiftTests.swift
+//  SupabaseSwiftTests
+//
+//  Created by Parham on 01/08/2025.
+//
+
+import Testing
+@testable import SupabaseSwift
+
+struct SupabaseSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
